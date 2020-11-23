9fe4057e3fb2a8a9abe593e1bd777846ba0c2c1e
BRE15150326 <C-brewer@hotmail.co.uk>
2020-11-23 17:10:03 +0000

    Room Designs and Setups
    Layed out some basic rooms that have wallls and passage ways, ready to be put into an array of different rooms for the generator to use.

---
+++ Erebor/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    //Labels Direction of Spawnpoint to contain a corresponding door
+    public int openingDirection;
+    //1 needs door down     v
+    //2 needs door left     <  
+    //3 needs door up       ^
+    //4 needs door right    >
+
+    private void Update()
+    {
+        if(openingDirection == 1){
+
+        }else if(openingDirection == 2){
+
+        }else if(openingDirection == 3){
+
+        }else if(openingDirection == 4){ 
+
+        }
+    }
+
+}
